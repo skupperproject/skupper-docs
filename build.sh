504804afe947ee34fd49d5f761f7a6d2cc5de8c7
Paul Wright <pwright@redhat.com>
2021-04-16 19:15:46 +0100

    init

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+DOCSEARCH_ENABLED=true DOCSEARCH_ENGINE=lunr NODE_PATH="$(npm -g root)" antora --generator antora-site-generator-lunr preview-playbook.yml
+echo "Antora build completed successfully."
+
+echo "Customizing output."
+# find $OUTPUT_DIR/dist -name '*.html' -exec sed -i 's/_images/assets-images/g' {} \;
+echo "Done."
