81228ea82c885962984b9a3671941164823439db
Paul Wright <pwright@redhat.com>
2022-05-09 13:23:10 +0100

    update

--- modules/cli/pages/create-didact.sh
+++
@@ -1,6 +0,0 @@
-# requires 'npm i -g autodidact'
-autodidact openshift.adoc 
-
-cat openshift.adoc.didact.adoc | sed -E 's/:sectnums:/:image-prefix:/g' > openshift.didact.adoc
-
-rm openshift.adoc.didact.adoc
