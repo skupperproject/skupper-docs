13214a70a9aa34309b307226c03a457b153f6252
Paul Wright <pwright@redhat.com>
2025-03-10 19:17:31 +0000

    update

--- input/main.js
+++ input/main.js
@@ -1,193 +1,179 @@
-// Licensed to the Apache Software Foundation (ASF) under one
-// or more contributor license agreements.  See the NOTICE file
-// distributed with this work for additional information
-// regarding copyright ownership.  The ASF licenses this file
-// to you under the Apache License, Version 2.0 (the
-// "License"); you may not use this file except in compliance
-// with the License.  You may obtain a copy of the License at
-//
-//   http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing,
-// software distributed under the License is distributed on an
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-// KIND, either express or implied.  See the License for the
-// specific language governing permissions and limitations
-// under the License.
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+"use strict";
 
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 Element.prototype.$ = function () {
-    return this.querySelector.apply(this, arguments);
+  return this.querySelector.apply(this, arguments);
 };
 
 Element.prototype.$$ = function () {
-    return this.querySelectorAll.apply(this, arguments);
+  return this.querySelectorAll.apply(this, arguments);
 };
 
-// window.addEventListener("load", () => {
-//     const oldToc = $("#-toc");
+window.addEventListener("load", () => {
+    let path = window.location.pathname;
+    let child = $("#-left-site-nav").firstChild;
 
-//     if (!oldToc) {
-//         return;
-//     }
+    if (path.charAt(path.length - 1) === "/") {
+        path += "index.html";
+    }
 
-//     const headings = $$("h2");
+    while (child) {
+        if (child.nodeType === 1 && child.id !== "-logotype") {
+            let prefix = new URL(child.href).pathname.slice(0, -10);
 
-//     if (headings.length == 0) {
-//         oldToc.remove();
-//         return;
-//     }
+            if (path.startsWith(prefix)) {
+                child.classList.add("selected");
+            }
+        }
 
-//     const newToc = document.createElement("section");
-//     const newTocHeading = document.createElement("h4");
-//     const newTocHeadingText = document.createTextNode("Contents");
-
-//     newTocHeading.appendChild(newTocHeadingText);
-//     newToc.appendChild(newTocHeading);
-//     newToc.setAttribute("id", "-toc");
-
-//     const newTocLinks = document.createElement("nav");
-
-//     const topLink = document.createElement("a");
-//     const topText = document.createTextNode("Top");
-
-//     topLink.setAttribute("href", "#");
-//     topLink.appendChild(topText);
-
-//     newTocLinks.appendChild(topLink);
-
-//     for (const heading of headings) {
-//         const link = document.createElement("a");
-//         const text = document.createTextNode(heading.textContent);
-
-//         link.setAttribute("href", `#${heading.id}`);
-//         link.appendChild(text);
-
-//         newTocLinks.appendChild(link);
-
-//         const subheadings = heading.parentElement.$$("h3");
-
-//         if (subheadings.length == 0) {
-//             continue;
-//         }
-
-//         const sublinks = document.createElement("nav");
-
-//         for (const subheading of subheadings) {
-//             const sublink = document.createElement("a");
-//             const subtext = document.createTextNode(subheading.textContent);
-
-//             sublink.setAttribute("href", `#${subheading.id}`);
-//             sublink.appendChild(subtext);
-
-//             sublinks.appendChild(sublink);
-//         }
-
-//         newTocLinks.appendChild(sublinks);
-//     }
-
-//     newToc.appendChild(newTocLinks);
-//     oldToc.replaceWith(newToc);
-// });
+        child = child.nextSibling;
+    }
+});
 
 window.addEventListener("load", () => {
-    const tocLinks = $("#-toc nav");
+    let oldTocLinks = $("#-toc > div");
+
+    if (!oldTocLinks) {
+        return;
+    }
+
+    let headings = $$("h2");
+
+    if (headings.length == 0) {
+        return;
+    }
+
+    let newTocLinks = document.createElement("div");
+
+    for (let heading of headings) {
+        let link = document.createElement("a");
+        let text = document.createTextNode(heading.textContent);
+
+        link.setAttribute("href", `#${heading.id}`);
+        link.appendChild(text);
+
+        newTocLinks.appendChild(link);
+    }
+
+    oldTocLinks.parentNode.replaceChild(newTocLinks, oldTocLinks);
+
+    $("#-toc").style.display = "block";
+});
+
+window.addEventListener("load", () => {
+    let tocLinks = $("#-toc > div");
 
     if (!tocLinks) {
         return;
     }
 
-    const updateHeadingSelection = () => {
-        const currHash = window.location.hash;
+    let updateHeadingSelection = () => {
+        let currHash = window.location.hash;
 
-	for (const element of $$(".selected")) {
-	    element.classList.remove("selected");
-	}
+        if (!currHash) {
+            return;
+        }
 
-        if (currHash) {
-            for (const link of tocLinks.$$("a")) {
-                const linkHash = new URL(link.href).hash;
+        for (let link of tocLinks.$$("a")) {
+            let linkHash = new URL(link.href).hash;
 
-                if (linkHash === currHash) {
-                    link.classList.add("selected");
-		    break;
-                }
+            if (linkHash === currHash) {
+                link.classList.add("selected");
+            } else {
+                link.classList.remove("selected");
             }
-
-	    $(currHash).parentElement.parentElement.classList.add("selected");
-	} else {
-            const link = tocLinks.$("a");
-
-            link.classList.add("selected");
         }
     }
 
-    updateHeadingSelection();
-
+    window.addEventListener("load", updateHeadingSelection);
     window.addEventListener("hashchange", updateHeadingSelection);
 });
 
 window.addEventListener("load", () => {
-    for (const block of $$("pre > code.language-console")) {
-	const lines = block.innerHTML.split("\n");
+    let updateScrollState = () => {
+        if (window.scrollY > 20) {
+            $("body").classList.add("scrolled");
+        } else {
+            $("body").classList.remove("scrolled");
+        }
+    };
 
-	block.innerHTML = lines.map(line => {
-	    switch (line[0]) {
-	    case "#":
-		return `<span class="shell-comment">${line}</span>`;
-	    case "$":
-		return `<span class="shell-command">${line}</span>`;
-	    default:
-		return `<span class="shell-output">${line}</span>`;
-	    }
-	}).join("\n");
+    updateScrollState();
+    window.addEventListener("scroll", updateScrollState);
+});
+
+window.addEventListener("load", () => {
+    let pathNav = $("#-path-nav");
+
+    if (pathNav.$$("a").length > 1) {
+        $("section:first-of-type > div").style.paddingTop = "9.5em";
+        $("#-toc").style.top = "9.5em";
+        $("html").style.scrollPaddingTop = "9em";
+
+        pathNav.style.display = "inherit";
     }
 });
 
 window.addEventListener("load", () => {
-    for (const element of $$("div.attribute > div.attribute-heading")) {
-	element.addEventListener("click", () => {
-	    element.parentElement.classList.toggle("collapsed");
-	});
-    }
+    let button = $("#-site-menu-button");
+    let layer = $("#-site-menu-layer");
+
+    button.addEventListener("click", () => {
+        layer.style.display = "inherit";
+    });
+
+    layer.addEventListener("click", (e) => {
+        if (e.target === layer) {
+            layer.style.display = "none";
+        }
+    });
 });
 
-window.addEventListener("load", () => {
-    if ($("a#expand-all")) {
-	$("a#expand-all").addEventListener("click", () => {
-	    for (const element of $$("div.attribute.collapsed")) {
-		element.classList.remove("collapsed");
-	    }
-	});
-    }
+if (navigator.clipboard) {
+    window.addEventListener("load", () => {
+        for (let pre of $$("pre")) {
+            const code = pre.$("code");
 
-    if ($("a#collapse-all")) {
-	$("a#collapse-all").addEventListener("click", () => {
-	    for (const element of $$("div.attribute")) {
-		element.classList.add("collapsed");
-	    }
-	});
-    }
-});
+            if (!code) continue;
 
+            if (code.textContent.trim().startsWith("$")) continue;
 
-// Function to open an image in fullscreen
-function openFullscreen(element) {
-  if (element.requestFullscreen) {
-    element.requestFullscreen();
-  } else if (element.mozRequestFullScreen) { // Firefox
-    element.mozRequestFullScreen();
-  } else if (element.webkitRequestFullscreen) { // Chrome, Safari, and Opera
-    element.webkitRequestFullscreen();
-  } else if (element.msRequestFullscreen) { // IE/Edge
-    element.msRequestFullscreen();
-  }
+            const button = document.createElement("a");
+            button.classList.add("copy-button");
+
+            const span = document.createElement("span");
+            span.textContent = "content_copy";
+            span.classList.add("material-icons");
+
+            button.appendChild(span);
+            pre.insertBefore(button, pre.firstChild);
+
+            button.addEventListener("click", () => {
+                navigator.clipboard.writeText(code.textContent.trim());
+            });
+        }
+    });
 }
-// Attach click event listeners to all img elements
-document.querySelectorAll('img').forEach(img => {
-  img.addEventListener('click', () => {
-    openFullscreen(img);
-  });
-});
