e2bd0e250394f9e4eaa3eea5f6cdf82ec4cb7f59
Paul Wright <pwright@redhat.com>
2025-03-19 15:43:04 +0000

    test menu

--- config/config.py
+++ config/config.py
@@ -1,10 +1,12 @@
 from plano import *
+import os
 
 site.output_dir = "output"
+site_prefix = os.environ.get("SITE_PREFIX", "")
 
 def path_nav(page):
     files = reversed(list(page.ancestors)[:-1])
-    links = [f"<a href=\"{x.url}\">{x.title}</a>" for x in files]
+    links = [f"<a href=\"{site_prefix}{x.url}\">{x.title}</a>" for x in files]
     links = " <span class=\"path-separator\">&#8250;</span> ".join(links)
 
     return f"<nav id=\"-path-nav\"><div>{links}</div></nav>"
